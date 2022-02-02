7646416a911ecdb4391bb2da45de605f4fa132a6
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-02-02 19:10:07 -0300

    getStaticProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,13 +1,50 @@
+import { useEffect, useState } from 'react'
 import Link from '../src/components/Link'
 
-export default function FAQPage() {
+export async function getStaticProps() {
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
+
+    return {
+        props: {
+            faq
+        }
+    }
+}
+
+export default function FAQPage({ faq }) {
+    console.log(faq);
+    
+    // const [faq, setFaq] = useState([]);
+
+    // useEffect(() => {
+
+    // }, []);
+
     return (
-        <div>
+        <>
             <h1>Alura Cases - Página de Perguntas FAQ</h1>
             
             <Link href="/">
                 Ir para a Home
             </Link>
-        </div>
+
+            <ul>
+                {faq.map(({ answer, question }) => ( 
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
