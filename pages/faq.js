71fde1db47ce20c73234a8ac1a5dff0fa839ea88
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-02-02 20:14:43 -0300

    next.config, redirecionamento e FAQScreen

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,8 @@
-import Link from '../src/components/Link'
+import FAQScreen from '../src/screens/FAQScreen';
 
-export async function getServerSideProps() {
+export default FAQScreen; 
+
+export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     
     const faq = await fetch(FAQ_API_URL)
@@ -16,27 +18,4 @@ export async function getServerSideProps() {
             faq
         }
     }
-}
-
-export default function FAQPage({ faq }) {
-    return (
-        <>
-            <h1>Alura Cases - Página de Perguntas FAQ</h1>
-            
-            <Link href="/">
-                Ir para a Home
-            </Link>
-
-            <ul>
-                {faq.map(({ answer, question }) => ( 
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
 }
