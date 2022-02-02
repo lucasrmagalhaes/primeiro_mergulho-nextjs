92386bf86929174e78f8f5d52d02944aaa947318
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-02-01 23:23:18 -0300

    Iniciando o projeto

---
+++ README.md
@@ -0,0 +1,17 @@
+**Next.js:** Primeiro mergulho no framework
+
+```
+npm install --global yarn
+```
+
+```
+yarn init -y
+```
+
+```
+yarn add next react react-dom
+```
+
+```
+npx gitignore node
+```
---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Página de Perguntas FAQ</h1>
+            <a href="/">Ir para a Home</a>
+        </div>
+    )
+}
